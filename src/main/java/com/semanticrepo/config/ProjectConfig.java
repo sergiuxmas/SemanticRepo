d0a369da0621911200185f50e2bfdcb17930e912
sergiuxmas <sergiu.xmas@gmail.com>
2026-02-11 17:16:12 +0200

    Set up customised SecurityFilterChain

--- src/main/java/com/semanticrepo/config/ProjectConfig.java
+++ src/main/java/com/semanticrepo/config/ProjectConfig.java
@@ -2,14 +2,23 @@ package com.semanticrepo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
+
+    private final CustomAuthenticationProvider authenticationProvider;
+
+    public ProjectConfig(CustomAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -35,5 +44,14 @@ public class ProjectConfig {
     public PasswordEncoder passwordEncoder() {
 //        return NoOpPasswordEncoder.getInstance();
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(authenticationProvider);
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+
+        return http.build();
     }
 }
